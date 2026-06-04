534661dbfb949d0778095854fc6f35ba66a9aa98
Alex ImAdmiringU <jezzzawot@gmail.com>
2026-06-04 12:48:51 +0300

    GB: initial commit

---
+++ ensembles/gradient_boosting.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+class BaseGradientBoosting:
+    def __init__(self):
+        pass
+
+
+class GradientBoostingClassifier(BaseGradientBoosting):
+    pass
+
+
+class GradientBoostingRegressor(BaseGradientBoosting):
+    pass
